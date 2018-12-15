8a1d2ec3c8271bbab8ac5e8a8e2bad73e7ed147a
Owen Henley <owenhenley@me.com>
2018-12-14 22:52:01 -0700

    initial project commit. Tab Bar Controller started.

---
+++ Podcasts/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  Podcasts
+//
+//  Created by Owen Henley on 12/14/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+    
+    
+}
